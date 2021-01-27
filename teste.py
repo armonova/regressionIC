c12d5539d80c5ea14a1693f6fa756ade894b6640
Arthur Novaes <arthur.novaes@enacom.com.br>
2021-01-27 02:12:52 -0300

    finishing the second exercise

--- teste.py
+++ teste.py
@@ -1,43 +1,47 @@
-
-# Making the imports
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 plt.rcParams['figure.figsize'] = (12.0, 9.0)
 
-# Preprocessing Input data
-data = pd.read_csv('data1.txt')
-X = data.iloc[:, 0]
-Y = data.iloc[:, 1]
-plt.scatter(X, Y)
-plt.show()
+data = pd.read_csv('data2.txt')
+X0 = data.iloc[:, 0]
+X1 = data.iloc[:, 1]
+Y = data.iloc[:, 2]
 
+# número de features
+theta_0 = 1
+theta_1 = 1
+theta_2 = 1
 
+alpha = 0.0000001  # taxa de aprendizado
+epochs = 100  # número de iterações
+m = float(len(X0))  # números de elementos de X
 
+loss_function_values = []
 
-# Building the model
-m = 0
-c = 0
-
-L = 0.0001  # The learning Rate
-epochs = 1000  # The number of iterations to perform gradient descent
-
-n = float(len(X))  # Number of elements in X
-
-# Performing Gradient Descent
 for i in range(epochs):
-    Y_pred = m * X + c  # The current predicted value of Y
-    D_m = (-2 / n) * sum(X * (Y - Y_pred))  # Derivative wrt m
-    D_c = (-2 / n) * sum(Y - Y_pred)  # Derivative wrt c
-    m = m - L * D_m  # Update m
-    c = c - L * D_c  # Update c
+    Y_pred = theta_2 * X1 + theta_1 * X0 + theta_0  # Valor previsto para Y
+    D_a1 = (-1 / m) * sum(X0 * (Y - Y_pred))  # derivada do theta_2
+    D_a2 = (-1 / m) * sum(X1 * (Y - Y_pred))  # derivada do theta_1
+    D_y = (-1 / m) * sum(Y - Y_pred)  # Derivada do theta_0
+    theta_1 = theta_1 - (alpha * D_a1)  # Atualiza o theta_2
+    theta_2 = theta_2 - (alpha * D_a2)  # Atualiza o theta_1
+    theta_0 = theta_0 - (alpha * D_y)  # Atualiza o theta_0
+    err = 0
+    for index, e in enumerate(X0):
+        err = err + (Y_pred[index] - Y[index]) ** 2
+    loss_function_values.append(1 / (2 * m) * err)
 
-print(m, c)
+print("Theta 2: " + str(theta_2) + "\nTheta 1: " + str(theta_1) + "\nTheta 0: " + str(theta_0))
 
+# Criando a função Y
+Y_pred = theta_2 * X1 + theta_1 * X0 + theta_0  # Valor previsto para Y
 
-# Making predictions
-Y_pred = m*X + c
+# Plotando os valores com a regressão
+ax = plt.axes(projection='3d')
+ax.scatter3D(X0, X1, Y, c=Y)
+# start | lenght | end
+ax.plot3D([min(X0), max(X0)], [min(X1), max(X1)], [min(Y_pred), max(Y_pred)], color='red')  # regression line
 
-plt.scatter(X, Y)
-plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color='red')  # regression line
 plt.show()
